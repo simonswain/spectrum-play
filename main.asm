91aff649a836643c8c59633fe00bee7e201749a1
Simon Swain <simon@simonswain.com>
2019-01-06 20:30:40 +1100

    Attrs for player sprite

--- main.asm
+++ main.asm
@@ -77,9 +77,11 @@ call draw_player
 ei
 reti
 
-;;;;;;;;
+player_sprite: db %11111111, %00111100, %00011000, %00111100, %00111100, %00011000, %00111100, %11111111
 
 screen_map: dw $4000, $4020, $4040, $4060, $4080, $40A0, $40C0, $40E0, $4800, $4820, $4840, $4860, $4880, $48A0, $48C0, $48E0, $5000, $5020, $5040, $5060, $5080, $50A0, $50C0, $50E0
+
+attrs_map: dw $5800, $5820, $5840, $5860, $5880, $58A0, $58C0, $58E0, $5900, $5920, $5940, $5960, $5980, $59A0, $59C0, $59E0, $5A00, $5A20, $5A40, $5A60, $5A80, $5AA0, $5AC0, $5AE0
 
 char_xy_to_pixel_mem:
 ; bc is y, x
@@ -87,6 +89,22 @@ ld h, 0
 ld l, b ; hl = Y
 add hl, hl ; hl = y*2
 ld de, screen_map
+add hl, de ; hl = screen_map + (row * 2)
+ld a, (hl) ; implements ld hl, (hl)
+inc hl
+ld h,(hl)
+ld l, a ; hl = address of first pixel from screen_map
+ld d, 0
+ld e, c
+add hl, de
+ret
+
+char_xy_to_attrs_mem:
+; bc is y, x
+ld h, 0
+ld l, b ; hl = Y
+add hl, hl ; hl = y*2
+ld de, attrs_map
 add hl, de ; hl = screen_map + (row * 2)
 ld a, (hl) ; implements ld hl, (hl)
 inc hl
@@ -109,8 +127,9 @@ ld c, a
 ret
 
 clear_player:
-call get_player_xy
+call get_player_xy ; bc now has y, x
 call char_xy_to_pixel_mem
+;; erase sprite
 ld c, 8
 clear_player_loop:
 ld a, 0
@@ -120,10 +139,10 @@ dec c
 jr nz, clear_player_loop
 ret
 
-player_sprite: db %11111111, %00111100, %00011000, %00111100, %00111100, %00011000, %00111100, %11111111
-
 draw_player:
-call get_player_xy
+call get_player_xy; bc now has y, xcall char_xy_to_pixel_mem
+
+push bc
 call char_xy_to_pixel_mem
 ld c, 8 ; 8 rows
 ld de, player_sprite ; sprite
@@ -134,6 +153,9 @@ inc h ; next pixel line
 inc e ; next sprite line
 dec c ; loop counter
 jr nz, draw_player_loop
+pop bc
+call char_xy_to_attrs_mem
+ld (hl), %01000111
 ret
 
 right:
